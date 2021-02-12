a20ead4105ac250af5d9a478a14901b70ea516db
zasd123 <62350166+zasd123@users.noreply.github.com>
2021-02-12 10:28:58 -0500

    Delete settings directory

--- settings/local_dev.py
+++
@@ -1,5 +0,0 @@
-import os
-
-# Set environment variables
-os.environ['API_URL'] = 'http://django-py3-staging.us-east-1.elasticbeanstalk.com/api/campaign-search/'
-os.environ['API_TOKEN'] = 'b5fb43bec67a715b6fadd03ff7fb85876fa34040'
